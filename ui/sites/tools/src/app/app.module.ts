c6189db9d59c8acf17f92102c041bf4d9d3a2423
Benjamin Simpson <ben@bensimpson.ch>
2022-12-09 10:26:56 +0100

    added basic development tools components, home page; added material

--- ui/sites/tools/src/app/app.module.ts
+++ ui/sites/tools/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ThemeModule} from "./theme/theme.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, BrowserAnimationsModule, AppRoutingModule, ThemeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
