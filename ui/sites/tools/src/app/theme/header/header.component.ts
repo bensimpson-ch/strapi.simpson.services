8a02373b1a97583fe10c9981009865ce164c60a0
Benjamin Simpson <ben@bensimpson.ch>
2022-12-12 22:11:45 +0100

    initial docs; update prod api url

--- ui/sites/tools/src/app/theme/header/header.component.ts
+++ ui/sites/tools/src/app/theme/header/header.component.ts
@@ -2,11 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Content} from "./content";
 import {ContentService} from "../../services/content.service";
 
-const en = 'en';
-const it = 'it';
-const fr = 'fr';
-const de = 'de';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -27,20 +22,13 @@ export class HeaderComponent implements OnInit{
     if (locale === null) {
       locale = navigator.language;
     }
-    if (locale.startsWith(de)) {
-      locale = de
-    } else if (locale.startsWith(fr)) {
-      locale = fr
-    } else if (locale.startsWith(it)) {
-      locale = it
-    } else  {
-      locale = en
+    if (locale.length > 2) {
+      locale = locale.substring(0,2);
     }
     localStorage.setItem('locale', locale);
   }
 
   private loadContent() {
     this.contentService.fetchContent(this.content);
-
   }
 }
--- ui/sites/tools/src/environments/environment.prod.ts
+++ ui/sites/tools/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //override this for local development https://strapi.simpson.services/api or http://localhost:1337/api
-  api: "https://strapi.simpson.services/api"
+  api: "https://strapi.simpson.services/api/"
 };
